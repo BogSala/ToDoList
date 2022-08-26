88d1255a3c5ca235d0ea73ac596aff2d90cf2f43
BogSala <zero959512@gmail.com>
2022-08-26 18:34:02 +0300

    registration and login on php side

--- ToDoList/model/Model.php
+++ ToDoList/model/Model.php
@@ -6,7 +6,40 @@ class Model
 
     function __construct()
     {
-        $this->db = new SQLite3("test.db");
+        // $this->db = new SQLite3("test.db");
+        $this->pdo = new PDO('sqlite:ToDoList.db');
+    }
+
+    public function returnAll()
+    {
+        $result = $this->pdo->query("SELECT * FROM users")->fetchAll();
+        return $result;
+    }
+
+    public function userAdd($login , $password)
+    {   
+        $this->pdo->prepare('INSERT INTO users (login, password) VALUES (?, ?)')->execute([$login, $password]);
+        if ($this->pdo->prepare("SELECT * FROM users WHERE login=?")->execute([$login])){
+            return true;
+        };
+        // return $result;
+    }
+
+    public function userLogin($login , $password)
+    {
+        try {
+            //? Без захисту від прямих ін'єкцій бо я можу собі дозволити
+            $createdUser = $this->pdo->query("SELECT * FROM users WHERE login = '$login' AND password = '$password'")->fetchAll();
+            
+        } catch (Exception $e){
+            return 'Eror:'.  $e->getMessage()."\n";
+        };
+
+        if (!$createdUser) {
+            return false;
+        };
+
+        return($createdUser);
     }
 
 }
